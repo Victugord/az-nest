88fecca12d08008a9ff3f286c8b6cc32221cd1ab
diogo adao <diogo.adao@visual-thinking.pt>
2022-11-16 17:29:29 +0000

    fixed tests

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,4 +9,8 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+  @Get("/world")
+  getWorld(): string {
+    return this.appService.getWorld();
+  }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -2,6 +2,9 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
+  getWorld(): string {
+    return "az function world is amazing"
+  }
   getHello(): string {
     return 'Hello World!';
   }
